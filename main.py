a0c0866453f744a36963a4571130ff55f92bcb52
Chris <80273421+CodeMorera@users.noreply.github.com>
2022-02-01 02:35:43 +0000

    Added a bill

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+bill=0
 if height >= 120:
   print("You can ride the rollercoaster")
-  age=int(input("What is your age?"))
-  if age < 12:
-    print("Please pay $5.")
-  elif age>= 12 & age < 18:
-    print("Please pay $7")
+  age=int(input("What is your age? "))
+  if age <= 12:
+    bill=5
+    print("Child tickets are $5. ")
+  elif age <= 18:
+    bill=7
+    print("Teen tickets are $7. ")
   else:
-    print("Please pay $12.")
+    bill=12
+    print("Adult tickets are $12. ")
+
+  photo_taken=input("Do you want a photo taken? Y or N ")
+  if photo_taken == "Y":
+    #Adds $3 to bill
+    bill += 3
+  print(f"Your final bill is ${bill}")
 else:
   print("Sorry, you need to grow taller")
